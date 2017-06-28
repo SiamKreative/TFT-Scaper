dcc06af7e19340dd827df10dffe5b67cb95dc450
Julien Vernet <julien@vernet.me>
2017-06-29 00:13:37 +0700

    Get image dimensions. See #2

--- inject.js
+++ inject.js
@@ -28,13 +28,21 @@ $(function () {
 			var table = $(el).tableToJSON({
 				ignoreEmptyRows: true,
 				headings: headings,
-				textExtractor: {
+				extractor: {
 					3: function (cellIndex, $cell) {
-						// Get the image src attribute
-						var imgUrl = $cell.find('a.image>img').attr('src');
-						// Get bigger image URL
-						if (imgUrl) {
-							return imgUrl.replace('110px-', '640px-');
+						var $img = $cell.find('img');
+						var $imgSrc = $img.attr('src');
+						if ($imgSrc) {
+							var ratio = $img.data('file-width') / $img.data('file-height');
+							var largeWidth = 640;
+							return {
+								thumbnailUrl: $imgSrc,
+								thumbnailWidth: $img.width(),
+								thumbnailHeight: $img.height(),
+								largeUrl: $imgSrc.replace('110px-', largeWidth + 'px-'),
+								largeWidth: largeWidth,
+								largeHeight: Math.round(largeWidth / ratio)
+							};
 						}
 					}
 				}
